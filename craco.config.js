dbf9c9a9cdafc167c439f0a4bb917f84a454578f
ayoung <zeusandninja@gmail.com>
2022-01-26 11:16:16 -0800

    Format everything with Prettier

--- craco.config.js
+++ craco.config.js
@@ -1,22 +1,26 @@
 module.exports = {
   webpack: {
-      configure: (webpackConfig, {env, paths}) => {
-          return {
-              ...webpackConfig,
-              entry: {
-                  main: [env === 'development' && require.resolve('react-dev-utils/webpackHotDevClient'),paths.appIndexJs].filter(Boolean),
-                  background: './src/scripts/background.ts',
-                  content: './src/scripts/content.ts',
-              },
-              output: {
-                  ...webpackConfig.output,
-                  filename: 'static/js/[name].js',
-              },
-              optimization: {
-                  ...webpackConfig.optimization,
-                  runtimeChunk: false,
-              }
-          }
-      },
-  }
-}
+    configure: (webpackConfig, { env, paths }) => {
+      return {
+        ...webpackConfig,
+        entry: {
+          main: [
+            env === "development" &&
+              require.resolve("react-dev-utils/webpackHotDevClient"),
+            paths.appIndexJs,
+          ].filter(Boolean),
+          background: "./src/scripts/background.ts",
+          content: "./src/scripts/content.ts",
+        },
+        output: {
+          ...webpackConfig.output,
+          filename: "static/js/[name].js",
+        },
+        optimization: {
+          ...webpackConfig.optimization,
+          runtimeChunk: false,
+        },
+      };
+    },
+  },
+};
